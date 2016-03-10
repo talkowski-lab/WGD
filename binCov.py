b34767bb6b08a992307dc30f1da40329b1026cf4
RCollins13 <Ryan Collins>
2016-03-10 13:48:44 -0500

    Redo

--- binCov.py
+++ binCov.py
@@ -147,7 +147,7 @@ def main():
         ncoverage = coverage.to_dataframe(names = 'chr start end cov'.split())
         medcov = ncoverage.loc[ncoverage['cov'] > 0, 'cov'].median()
         ncoverage['cov'] = ncoverage['cov'] / medcov
-        ncoverage.to_csv(args.norm_out, sep='\t', index=False, header=False)
+        ncoverage.to_csv(args.norm_out[0], sep='\t', index=False, header=False)
         call(' '.split(['sort -Vk1,1 -k2,2n -o', args.norm_out, 
                         args.norm_out]), shell=True)
 
