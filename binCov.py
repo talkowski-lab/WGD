8f604990ef505116b37cd9399f8f999a59e89a17
RCollins13 <Ryan Collins>
2016-03-10 13:47:27 -0500

    Undo

--- binCov.py
+++ binCov.py
@@ -100,7 +100,7 @@ def binCov(bam, chr, binsize, mode='nucleotide', overlap=0.05, blacklist=None):
     bins = pybedtools.BedTool(bins)
 
     #Remove bins that have at least 5% overlap with blacklist by size
-    blist = pybedtools.BedTool(blacklist)
+    blist = pybedtools.BedTool(blacklist[0])
     bins_filtered = bins.intersect(blist, v=True, f=overlap)
 
     #Filter bam
