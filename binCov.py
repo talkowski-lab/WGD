3929b4e99b99e6d6a495d3b9703ecc2456d6789a
RCollins13 <Ryan Collins>
2016-03-10 13:35:22 -0500

    Add missing bracket

--- binCov.py
+++ binCov.py
@@ -149,7 +149,7 @@ def main():
         ncoverage['cov'] = ncoverage['cov'] / medcov
         ncoverage.to_csv(args.norm_out, sep='\t', index=False, header=False)
         call(' '.split(['sort -Vk1,1 -k2,2n -o', args.norm_out, 
-                        args.norm_out), shell=True)
+                        args.norm_out]), shell=True)
 
 
 #Main block
