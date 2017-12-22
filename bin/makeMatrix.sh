88fad8afc4185303fb7873a2f6c068e5052b77d2
Ryan L Collins <rlcollins@g.harvard.edu>
2017-12-22 12:25:44 -0500

    Add restriction list to makeMatrix.sh

--- bin/makeMatrix.sh
+++ bin/makeMatrix.sh
@@ -10,7 +10,7 @@
 usage(){
 cat <<EOF
 
-usage: makeMatrix.sh [-h] [-z] [-o OUTFILE] SAMPLES
+usage: makeMatrix.sh [-h] [-z] [-r RESTRICT] [-o OUTFILE] SAMPLES
 
 Helper tool to automate creation of sorted coverage matrices from
 binCov.py output bed files
@@ -19,17 +19,19 @@ Positional arguments:
   SAMPLES     List of samples and coverage files (tab-delimmed)
 
 Optional arguments:
-  -h  HELP      Show this help message and exit
-  -z  GZIP      Gzip output file
-  -o  OUTFILE   Output file (default: stdout)
+  -h  HELP        Show this help message and exit
+  -z  BGZIP       Bgzip and tabix output file
+  -r  RESTRICT    Restrict output to all bins in RESTRICT file
+  -o  OUTFILE     Output file (default: stdout)
 
 EOF
 }
 
 #Parse arguments
 OUTFILE=/dev/stdout
+RESTRICT=0
 GZ=0
-while getopts ":o:zh" opt; do
+while getopts ":o:r:zh" opt; do
 	case "$opt" in
 		h)
 			usage
@@ -37,6 +39,9 @@ while getopts ":o:zh" opt; do
 			;;
     z)
       GZ=1
+      ;;
+    r)
+      RESTRICT=${OPTARG}
       ;;
     o)
       OUTFILE=${OPTARG}
@@ -67,16 +72,27 @@ fi
 #Use bedtools to create matrix
 /usr/bin/env bedtools unionbedg \
   -header \
-  -names $( while read ID cov; do echo "${ID}"; done < ${SAMPLES} | paste -s -d\  )\
-  -i $( while read ID cov; do echo "${cov}"; done < ${SAMPLES} | paste -s -d\  )|\
-  sed -e 's/chrom/Chr/g' -e 's/start/Start/g' -e 's/end/End/g' > ${OUTFILE}
-  
-#Add hash to header line of OUTFILE
-cat <( head -n1 ${OUTFILE} | awk '{ print "#"$0 }' ) \
-    <( sed '1d' ${OUTFILE} ) > ${OUTFILE}2
-    mv ${OUTFILE}2 ${OUTFILE}
+  -names $( while read ID cov; do echo "${ID}"; done < ${SAMPLES} | paste -s -d\  ) \
+  -i $( while read ID cov; do echo "${cov}"; done < ${SAMPLES} | paste -s -d\  ) | \
+  sed -e 's/chrom/Chr/g' -e 's/start/Start/g' -e 's/end/End/g' | \
+  sort -Vk1,1 -k2,2n -k3,3n | uniq > ${OUTFILE}
 
-#Gzip OUTFILE, if optioned
+#Restrict OUTFILE to RESTRICT bins
+if [ ${RESTRICT} != "0" ]; then
+  head -n1 ${OUTFILE} > ${OUTFILE}2
+  awk '{ print $1"_"$2"_"$3 }' ${RESTRICT} | fgrep -wf - \
+  <( paste <( sed '1d' ${OUTFILE} | awk '{ print $1"_"$2"_"$3 }' ) \
+           <( sed '1d' ${OUTFILE} ) ) | cut -f2- >> ${OUTFILE}2
+  mv ${OUTFILE}2 ${OUTFILE}
+fi
+
+#Add hash to header line of OUTFILE & sort
+cat <( head -n1 ${OUTFILE} | awk '{ print "#"$0 }' ) \
+    <( sed '1d' ${OUTFILE} | sort -Vk1,1 -k2,2n -k3,3n | uniq ) > ${OUTFILE}2
+mv ${OUTFILE}2 ${OUTFILE}
+
+#Bgzip & tabix OUTFILE, if optioned
 if [ ${GZ}==1 ] && [ ${OUTFILE} != "/dev/stdout" ]; then
-  gzip -f ${OUTFILE}
+  bgzip -f ${OUTFILE}
+  tabix -f ${OUTFILE}.gz
 fi
